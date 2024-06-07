a0d31b418ef4ae9b76b7e2e7cfac4a5bc5276813
cccsssshh <shcho9666@gmail.com>
2024-06-07 21:55:20 +0900

    add launch file

---
+++ src/test_package/launch/drobot_3.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    package_share_directory = get_package_share_directory('test_package')
+    params_file = os.path.join(package_share_directory, 'coordinate.yaml')
+    
+    return LaunchDescription([
+        Node(
+            namespace='',
+            package='test_package',
+            executable='drobot_motor_3',
+            output='screen',
+            parameters=[params_file]
+        )
+    ])
